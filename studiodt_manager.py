fd756e782d5db1faccf624a2ec5985fe1e91af64
franzmelchiori <franzmelchiori@gmail.com>
2025-04-02 19:29:41 +0200

    up studiodt

---
+++ studiodt_manager.py
@@ -0,0 +1,22 @@
+"""
+    StudioDT Manager
+    Francesco Melchiori, 2025
+"""
+
+
+import pandas
+
+
+class PatientFolders:
+    def __init__(self):
+        pass
+
+    def retrive_folders():
+        pass
+
+    def check_folders():
+        pass
+
+
+if __name__ == '__main__':
+    pass
